18260a69cd97d4145c3ea544df5f386bff098f60
Apurbo Kumar Anup <apurboka@gmail.com>
2023-01-15 14:48:38 +0600

    Update

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -19,12 +19,60 @@ export const signUpUser = createAsyncThunk("signupuser", async (body) => {
   return await res.json();
 });
 
+export const signInUser = createAsyncThunk("signinuser", async (body) => {
+  const res = await fetch("http://103.91.228.10:5000/api/login", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return await res.json();
+});
+
 const authSlice = createSlice({
   name: "user",
   initialState,
-  reducersL: {},
+  reducersL: {
+    addToken: (state, action) => {
+      state.token = localStorage.getItem("token");
+    },
+    addUser: (state, action) => {
+      state.user = localStorage.getItem("user");
+    },
+    logout: (state, action) => {
+      state.token = null;
+      localStorage.clear();
+    },
+  },
 
   extraReducers: {
+    // Log in
+    [signInUser.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [signInUser.fulfilled]: (
+      state,
+      { paylod: { error, msg, token, user } }
+    ) => {
+      state.loading = false;
+      if (error) {
+        state.error = error;
+      } else {
+        state.msg = msg;
+        state.token = token;
+        state.user = user;
+
+        localStorage.setItem("msg", msg);
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("token", token);
+      }
+    },
+    [signInUser.rejected]: (state, action) => {
+      state.loading = true;
+    },
+
+    // Sign up
     [signUpUser.pending]: (state, action) => {
       state.loading = true;
     },
@@ -42,4 +90,5 @@ const authSlice = createSlice({
   },
 });
 
+export const { addToken, addUser, logout } = authSlice.actions;
 export default authSlice.reducer;
